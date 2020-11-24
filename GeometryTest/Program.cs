7d2d1fbc20c8ab08f548980eb7819d5d6a0262c4
Anton <48159735+antonalabin@users.noreply.github.com>
2020-11-24 16:04:58 +0300

    Добавьте файлы проекта.

---
+++ GeometryTest/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace GeometryTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const int n = 6;
+            Console.Write("Введите l: ");
+            double l = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Введите h: ");
+            double h = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Введите alpha: ");
+            double alpha = Convert.ToDouble(Console.ReadLine());
+
+            List<Point> points = Algo.GetPoints(l, h, alpha, n);
+
+            points.Add(new Point(0, 0));
+            Point center = new Point(l / 2, h / 2);
+            points = points.OrderBy(point => Math.Atan2(point.Y - center.Y, point.X - center.X)).ToList();
+
+            foreach (Point point in points)
+                Console.WriteLine(point.ToString());
+        }
+    }
+
+    public struct Point
+    {
+        public Point(double x, double y)
+        {
+            X = x;
+            Y = y;
+        }
+
+        public double X { get; }
+        public double Y { get; }
+
+        public override string ToString() => $"({X}, {Y})";
+
+        public override bool Equals(Object obj)
+        {
+            if ((obj == null) || !this.GetType().Equals(obj.GetType()))
+                return false;
+            else
+            {
+                double difference = 0.000001;
+                Point p = (Point)obj;
+                return ((Math.Abs(X - p.X) <= difference) && (Math.Abs(Y - p.Y) <= difference));
+            }
+        }
+    }
+
+    public static class Algo
+    {
+        public static List<Point> GetPoints(double l, double h, double alpha, int n)
+        {
+            if (l <= 0)
+                throw new ArgumentOutOfRangeException("l");
+            if (h <= 0)
+                throw new ArgumentOutOfRangeException("h");
+            if ((alpha <= 0) || (alpha >= 90))
+                throw new ArgumentOutOfRangeException("alpha");
+            if ((n < 6) || ((n - 2) % 4 != 0))
+                throw new ArgumentOutOfRangeException("n");
+
+            List<Point> points = new List<Point>(n + 2);
+
+            double t = Math.Tan(alpha * Math.PI / 180);
+            for (int i = 0; i <= n / 2; i++)
+            {
+                double x = l * i / n;
+                double y = (i % 2 == 0) ? (h + t * x) : 0;
+                points.Add(new Point(x, y));
+                points.Add((i != n / 2) ? new Point(l - x, y) : new Point(x, h + t * x));
+            }
+
+            return points;
+        }
+    }
+}
---
+++ XUnitTestProject1/UnitTest1.cs
@@ -0,0 +1,32 @@
+using GeometryTest;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Xunit;
+
+namespace XUnitTestProject1
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void GetPoints_ValidInput_ExpectedResult()
+        {
+            int n = 6;
+            double l = 12;
+            double h = 2;
+            double alpha = 45;
+            List<Point> expected = new List<Point>(8)
+            {
+                new Point(2,0), new Point(6,0), new Point(10, 0),
+                new Point(12, 2), new Point(8, 6), new Point(6,8), new Point(4,6), new Point(0, 2)
+            };
+            Point center = new Point(l / 2, h / 2);
+            expected = expected.OrderBy(point => Math.Atan2(point.Y - center.Y, point.X - center.X)).ToList();
+
+            List<Point> actual = Algo.GetPoints(l, h, alpha, n);
+            actual = actual.OrderBy(point => Math.Atan2(point.Y - center.Y, point.X - center.X)).ToList();
+
+            Assert.Equal(expected, actual);
+        }
+    }
+}
